0349bb1f441e00d58332561f1b5569679919397c
Grupo-Ocho <93555287+Grupo-Ocho@users.noreply.github.com>
2021-11-15 22:13:37 -0500

    Update CalcularMetabolismo.java

--- app/src/main/java/co/edu/unab/formsaludapp/CalcularMetabolismo.java
+++ app/src/main/java/co/edu/unab/formsaludapp/CalcularMetabolismo.java
@@ -8,29 +8,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class CalcularMetabolismo extends AppCompatActivity {
+
+    RadioButton rMasculino,rFemenino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calcular_metabolismo);
         setListeners();
+
+        rMasculino=(RadioButton) findViewById(R.id.radio_masculino);
+        rFemenino=(RadioButton) findViewById(R.id.radio_femenino);
     }
 
     private void setListeners() {
-        RadioGroup opcionSexo = findViewById(R.id.opciones_sexo);
-        int radioButtonId = opcionSexo.getCheckedRadioButtonId();
-        View radioButton = opcionSexo.findViewById(radioButtonId);
-        int idX = opcionSexo.indexOfChild(radioButton);
 
         Button buttonCalcular = findViewById(R.id.buttonCalcularMetabolismo);
         buttonCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+             
                 Double metabolismo = 0.0;
                 Double peso = Double.parseDouble(((EditText) findViewById(R.id.editTextPesoMeta)).getText().toString());
                 Double altura = Double.parseDouble(((EditText) findViewById(R.id.editTextTextEstaturaMeta)).getText().toString());
@@ -39,23 +41,19 @@ public class CalcularMetabolismo extends AppCompatActivity {
                 LinearLayout linearLayoutResultados = findViewById(R.id.llayoutResultadosMetabolismo);
                 linearLayoutResultados.setVisibility(View.VISIBLE);
 
+
                 double formula = (10 * peso) + (6.25 * altura) - (5 * edad);
 
-                if (idX==0){
-
-                    metabolismo = formula +5;
-
-                } else{
-
+                if (rMasculino.isChecked()==true) {
+                    metabolismo = formula + 5;
+                }else if (rFemenino.isChecked()){
                     metabolismo = formula - 161;
-
                 }
 
                 ((TextView) findViewById(R.id.txtEstaturaResultado)).setText("Altura: "+altura);
                 ((TextView) findViewById(R.id.txtPesoResultado)).setText("Peso: "+peso);
                 ((TextView) findViewById(R.id.txtEdadResultado)).setText("Edad: "+edad);
                 ((TextView) findViewById(R.id.txtMetabolismoResultado)).setText("Metabolismo: "+metabolismo);
-
 
             }
         });
